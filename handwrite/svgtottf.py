c32da5e80122d233fa56f045779976e6b65ed428
Kelsey Higham <kelseyhigham+github@gmail.com>
2025-07-25 20:13:18 -0700

    tweak

--- handwrite/svgtottf.py
+++ handwrite/svgtottf.py
@@ -266,9 +266,8 @@ creator   = ["''' + designer + '''"]
 license   = "''' + license + '''"
 ligatures = true
 ucsur     = true
-writing_system = "sitelen pona" # must be one of the following: 
-                                # "sitelen pona","sitelen sitelen","alphabet","syllabary","logography",
-                                # "tokiponido alphabet","tokiponido syllabary","tokiponido logography"
+writing_system = "sitelen pona" # pick one: sitelen pona, sitelen sitelen, alphabet, syllabary, logography,
+                                # tokiponido alphabet, tokiponido syllabary, tokiponido logography
 
 last_updated = "''' + datetime.now().strftime("%Y-%m") + '''"
 version      = "1"
