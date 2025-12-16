KelseyHigham/sp-font-maker

commit 8bd5d6d3a33d03e389274b8fc85f4c49202dc4a9

improve ligature alias support
- prevent ligature aliases from overriding a writein glyph's ligature, e.g. redirecting a writein ni2 to the default ni2
- enable default ligatures for writeins, e.g. complement mu2 with an equivalent la2

handwrite/add_ligatures.py
@@ -157,13 +157,21 @@ feature liga {
     # linuwi, kepen, ali, ni-numbers, space space, hyphen
     # todo: allow these to be overridden by custom-words. that would help kilitelen linuwi, and any font's ni2
     aliases = default_json_data.get("glyphs", {}).get("ligature-aliases")
+    glyphs = default_json_data.get("glyphs", {}).get("sheet")
     for alias in aliases:
-        list_of_ligs.append(
-            (
-                f"  sub   {(alias['ligature']).rjust(22)}   by   {(alias['target-name']).rjust(13)};",
-                len(alias["ligature"].split(" ")),
+        if (
+            # If the ligature alias's target exists
+            alias["target-name"] in cartoucheable_and_stackable
+            # If we're not redirecting a writein glyph's ligature to a default glyph
+            and alias["ligature"].replace(" ", "") + "Tok"
+            not in cartoucheable_and_stackable
+        ):
+            list_of_ligs.append(
+                (
+                    f"  sub   {(alias['ligature']).rjust(22)}   by   {(alias['target-name']).rjust(13)};",
+                    len(alias["ligature"].split(" ")),
+                )
             )
-        )
 
     # sort them by number of tokens
     list_of_ligs.sort(reverse=True, key=lambda x: x[1])

commit 6e12bb8cee6985d3a29e045874cac99f0ba0323f

fix stacked glyphs
they were broken in 8bd5d6d "improve ligature alias support"

handwrite/add_ligatures.py
@@ -63,7 +63,11 @@ feature liga {
 
     # Create ligature lines.
     with open(default_json) as f:
-        glyphs = json.load(f).get("glyphs", {}).get("sheet", [])
+        json_glyphs = json.load(f).get("glyphs", {})
+        glyphs_sheet = json_glyphs.get("sheet", [])
+        glyphs_derived = json_glyphs.get("derived", [])
+        glyphs_spaces = json_glyphs.get("spaces", [])
+        glyphs = glyphs_sheet + glyphs_derived + glyphs_spaces
         for k in glyphs:
             if "ligature" in k:
                 lig = k["ligature"]
@@ -127,6 +131,8 @@ feature liga {
                     # and k['name'] != "colonTok"
                     # and k['name'] != "teTok"
                     # and k['name'] != "toTok"
+                    and k["name"] != "stackJoinTok"
+                    and k["name"] != "ideographicspace"
                 ):
                     cartoucheable_and_stackable.append(k["name"])
 

handwrite/default.toml
@@ -389,9 +389,12 @@ glyphs.spaces = [
   {codepoint = 0x3b, width = 1000, name = "semicolon",   writein = ";"},
 
   # UCSUR features:
-  {codepoint = 0x3000,  width = 1000, name = "ideographicspace"},
+  # The `space space` ligature doesn't work in HTML, where consecutive
+  # whitespace is normally collapsed. In that case, type a space with `|`
+  # instead.
+  {codepoint = 0x3000,  width = 1000, name = "ideographicspace", ligature = "space space"},
   {codepoint = 0x200b,  width =    0, name = "zerowidthspace"},
-  {codepoint = 0xf1995, width =    0, name = "stackJoinTok"},
+  {codepoint = 0xf1995, width =    0, name = "stackJoinTok",     ligature = "hyphen"},
 
   # Fallback for unsupported UCSUR features:
   {codepoint = 0xf1996, width =    0, name = "nestJoinTok"},
@@ -435,12 +438,6 @@ ligature-aliases = [ # implemented!!!
   {ligature = "n i zero six",   target-name = "niTok.NE"},
   {ligature = "n i zero seven", target-name = "niTok.NW"},
   {ligature = "n i zero eight", target-name = "niTok.SW"},
-
-  {ligature = "hyphen",      target-name = "stackJoinTok"},
-
-  # This ligature doesn't work in HTML, where consecutive whitespace is
-  # normally collapsed. In that case, type a space with `|` instead.
-  {ligature = "space space", target-name = "ideographicspace"},
 ]
 
 